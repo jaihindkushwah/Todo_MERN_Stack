75f43e8ec48df619f2e9ee488032cd9343a6aff3
jaihindkushwah <jaihindkushwaha672001@gmail.com>
2024-04-26 23:42:54 +0530

    added all deployment logic

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,32 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 require("dotenv").config();
 require("./config/db");
 const userRouter = require("./routes/user");
 const taskRouter = require("./routes/task");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/api/user", userRouter);
 
 app.use("/api/task", taskRouter);
+
+const __dirname1=path.resolve();
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname1,"/frontend/build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+    });
+}else{
+    app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`));
